814db4c5aa4b39b580b0ad89de232a89669da2a8
Kohki SHIKATA <indigoworks@gmail.com>
2023-03-24 02:01:57 +0900

    Add onetime token

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+// セッションの利用を開始
+session_start();
+
+// ワンタイムトークン生成
+$toke_byte = openssl_random_pseudo_bytes(16);
+$csrf_token = bin2hex($toke_byte);
+
+// トークンをセッションに保存
+$_SESSION['csrf_token'] = $csrf_token;
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
